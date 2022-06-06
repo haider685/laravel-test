c07781e8679f35f4845aae7991abb037c21270b4
Abubakr Haider <abubakr.haider@alexatranslations.com>
2022-06-06 20:23:32 +0500

    returns all events with workshops

--- app/Models/Event.php
+++ app/Models/Event.php
@@ -4,7 +4,11 @@ namespace App\Models;
 
 
 use Illuminate\Database\Eloquent\Model;
-
+use App\Models\Workshop;
 class Event extends Model
 {
+	function workshops()
+	{
+		return $this->hasMany(Workshop::class)->where('event_id','>=',0);
+	}
 }
